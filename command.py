9df170127c154852bfdba5dd26329d2297601f27
lucas <Lucas Malta>
2018-07-18 09:44:02 +0200

    Adding total. Changing formating style to .format

--- command.py
+++ command.py
@@ -8,9 +8,11 @@ import re
 
 class Command(object):
     def __init__(self):
+        
         self.commands = { 
             "home" : self.home,
             "food" : self.food,
+            "total" : self.total,
             "help" : self.help
         }
         self.ammount = {'current': 0, 'food':0, 'home':0}
@@ -33,16 +35,22 @@ class Command(object):
               response += self.commands['home']()
             else:
               response += "Sorry, you need to provide a quantity."
+        
+        elif ('total' in command):
+          response += self.commands['total']()
              
         else:
             response += "Sorry I don't understand the command: " + command + ". " + self.help()
         return response
 
     def food(self):
-        return "OK! Adding " + str(self.ammount['current']) + " in food. Now total in: " + str(self.ammount['food'])
+        return "OK! Adding {} in food. New total is: {}".format(self.ammount['current'], self.ammount['food'])
          
     def home(self):
-        return "OK! Adding " + str(self.ammount['current']) + " in home. Now total in: " + str(self.ammount['home'])
+        return "OK! Adding {} in home. New total is: {}".format(self.ammount['current'], self.ammount['home'])
+    
+    def total(self):
+        return "Right. The total so far is: \n Food: {} \n Home: {}".format(self.ammount['food'], self.ammount['home'])
      
     def help(self):
         response = "Currently I support the following commands:\r\n"
