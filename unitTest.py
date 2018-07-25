10cb77f02fae11e6319673994f68bad43c76635c
lucas <Lucas Malta>
2018-07-25 12:16:57 +0200

    New tests for shopframe

--- unitTest.py
+++ unitTest.py
@@ -1,57 +1,93 @@
 #!/home/lucas/anaconda2/bin/python
 """
 Unit testing module
-Command test procedure
+Test procedure
 
 """
 
 import unittest
 import command
+import shopframe
+import pandas as pd
+
+##############################
+#  COMMAND TEST PROCEDURE    #
+##############################
+
+#myCom = command.Command()
 
 
-myCom = command.Command()  
+##############################
+#  SHOPFRAME TEST PROCEDURE  #
+##############################
 
-class FoodTestCase(unittest.TestCase):
-    """ Test case for command.py """
+# Create test data
+file_csv = '~/GIT/gotbot/test.csv'
+data = ['Lucas', 20180707, 0,0]
+df = pd.DataFrame([data], columns = ['User', 'Date', 'Food', 'Home'])
+df.to_csv(file_csv, index_label=False)
+
+# Read test data
+myshop = shopframe.ShopFrame(file_csv)  
+
+class AddPoint(unittest.TestCase):
+    """ Test case for shopframe.py -- Adding data """
   
-    def test_food_reply(self):
-        """ Test food """
-        self.assertEqual(myCom.handle_command('luc','food 10'), '<@luc>: OK! Adding 10 in food. New total is: 10')
+    def test_a_add_point(self):
+        """ Test add point """
+        self.assertEqual(myshop.add_data_point('Lucas','Food', 20180705, 10),0)
+
+    def test_b__get_grand_total_food(self):
+        """ Test total for food """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Food'], 10)
     
-    def test_no_ammount_food_reply(self):
-        """ Test food without specifying amomunt """
-        self.assertEqual(myCom.handle_command('luc','food'), '<@luc>: Sorry, you need to provide a quantity.')
+    def test_c_get_grand_total_home(self):
+        """ Test total for home """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Home'], 0)
+    
+    def test_d_add_point(self):
+        """ Test add point """
+        self.assertEqual(myshop.add_data_point('Lucas','Home', 20180705, 25),0)
+    
+    def test_e_get_grand_total_food(self):
+        """ Test total for food """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Food'], 10)
+    
+    def test_f_get_grand_total_home(self):
+        """ Test total for home """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Home'], 25)
 
-    def test_neg_ammount_food_reply(self):
-        """ Test food with negative amomunt """
-        self.assertEqual(myCom.handle_command('luc','food -10'), '<@luc>: OK! Adding -10 in food. New total is: 0')
+    def test_g_add_point(self):
+        """ Test add point """
+        self.assertEqual(myshop.add_data_point('Lucas','Food', 20180705, 10),0)
+    
+    def test_h_get_grand_total_food(self):
+        """ Test total for food """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Food'], 20)
+    
+    def test_i_get_grand_total_home(self):
+        """ Test total for home """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Home'], 25)
 
-
-
-class HomeTestCase(unittest.TestCase):
-    """ Test case for command.py """
-  
-    def test_home_reply(self):
-        """ Test home """
-        self.assertEqual(myCom.handle_command('luc','home 10'), '<@luc>: OK! Adding 10 in home. New total is: 10')
-
-    def test_no_ammount_home_reply(self):
-        """ Test home without specifying amomunt """
-        self.assertEqual(myCom.handle_command('luc','home'), '<@luc>: Sorry, you need to provide a quantity.')
-
-    def test_neg_ammount_home_reply(self):
-        """ Test home with negative amomunt """
-        self.assertEqual(myCom.handle_command('luc','home -10'), '<@luc>: OK! Adding -10 in home. New total is: 0')
-
-
-
-class TotalTestCase(unittest.TestCase):
-    """ Test case for command.py """
-  
-    def test_total_reply(self):
-        """ Test total """
-        self.assertEqual(myCom.handle_command('luc','total'), '<@luc>: Right. The total so far is: \n Food: 0 \n Home: 0')
-
+    def test_j_add_point(self):
+        """ Test add point """
+        self.assertEqual(myshop.add_data_point('Lucas','Food', 20180705, -30),0)
+    
+    def test_k_get_grand_total_food(self):
+        """ Test total for food """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Food'], -10)
+    
+    def test_l_get_grand_total_home(self):
+        """ Test total for home """
+        t = myshop.get_grand_total()
+        self.assertEqual(t['Home'], 25)
 
 
 
