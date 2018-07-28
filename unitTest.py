096da527b96692af9ad42cbc711d828f7858d1f2
lucas <Lucas Malta>
2018-07-28 10:17:49 +0200

    Adding tests to total by user

--- unitTest.py
+++ unitTest.py
@@ -16,7 +16,7 @@ import pandas as pd
 
 # Create test data
 file_csv = '~/GIT/gotbot/test.csv'
-data = [['lucas', 20180707, 0,0], ['lucas', 20180607, 10,0]]
+data = [['luc', 20180707, 0,0], ['ana', 20180607, 10,0]]
 df = pd.DataFrame(data, columns = ['user', 'date', 'food', 'home'])
 df.to_csv(file_csv, index_label=False)
 channel = 'CBTCHEDGE'
@@ -68,8 +68,20 @@ class AddCommand(unittest.TestCase):
         """ Total may 18 """
         self.assertEqual(myCom.handle_command('luc','total may 18', channel), '<@luc>: \nTotal for *May*, *2018*\nfood    0.0\nhome    0.0\n')
 
+    def test_l_total_me_june(self):
+        """ Total me June """
+        self.assertEqual(myCom.handle_command('ana','total me June', channel), '<@ana>: \nTotal for *June*, *2018*\nfood    10\nhome     0\n')
+        self.assertEqual(myCom.handle_command('luc','total me june', channel), '<@luc>: \nTotal for *June*, *2018*\nfood    0.0\nhome    0.0\n')
+    
+    def test_m_total_me_july(self):
+        """ Total me July """
+        self.assertEqual(myCom.handle_command('ana','total me july', channel), '<@ana>: \nTotal for *July*, *2018*\nfood    0.0\nhome    0.0\n')
+        self.assertEqual(myCom.handle_command('luc','total me july', channel), '<@luc>: \nTotal for *July*, *2018*\nfood    20\nhome    15\n')
 
-
+    def test_n_total_me(self):
+        """ Total me """
+        self.assertEqual(myCom.handle_command('luc','total me', channel), '<@luc>: \nGrand total (*ALL DATA*):\nfood    20\nhome    15\n')
+        self.assertEqual(myCom.handle_command('ana','total me', channel), '<@ana>: \nGrand total (*ALL DATA*):\nfood    10\nhome     0\n')
 
 
 
@@ -79,8 +91,8 @@ class AddCommand(unittest.TestCase):
 
 # Create test data
 file_csv = '~/GIT/gotbot/test.csv'
-data = ['lucas', 20180707, 0,0]
-df = pd.DataFrame([data], columns = ['user', 'date', 'food', 'home'])
+data = [['luc', 20180707, 0,0], ['ana', 20180607, 10,0]]
+df = pd.DataFrame(data, columns = ['user', 'date', 'food', 'home'])
 df.to_csv(file_csv, index_label=False)
 
 # Read test data
@@ -90,61 +102,76 @@ class AddPoint(unittest.TestCase):
     """ Test case for shopframe.py -- Adding data """
   
     def test_a_add_point(self):
-        """ Test add point """
-        self.assertEqual(myshop.add_data_point('lucas','food', 20180705, 10),0)
+        self.assertEqual(myshop.add_data_point('luc','food', 20180705, 10),0)
 
-    def test_b__get_grand_total_food(self):
-        """ Test total for food """
+    def test_b__get_grand_total(self):
         t = myshop.get_grand_total()
-        self.assertEqual(t['food'], 10)
-    
-    def test_c_get_grand_total_home(self):
-        """ Test total for home """
-        t = myshop.get_grand_total()
+        self.assertEqual(t['food'], 20)
         self.assertEqual(t['home'], 0)
     
-    def test_d_add_point(self):
-        """ Test add point """
-        self.assertEqual(myshop.add_data_point('lucas','home', 20180705, 25),0)
+    def test_c_add_point(self):
+        self.assertEqual(myshop.add_data_point('luc','home', 20180705, 25),0)
     
-    def test_e_get_grand_total_food(self):
-        """ Test total for food """
+    def test_d_get_grand_total(self):
         t = myshop.get_grand_total()
-        self.assertEqual(t['food'], 10)
+        self.assertEqual(t['food'], 20)
+        self.assertEqual(t['home'], 25)
+
+    def test_e_add_point(self):
+        self.assertEqual(myshop.add_data_point('luc','food', 20180705, 10),0)
     
-    def test_f_get_grand_total_home(self):
-        """ Test total for home """
+    def test_f_get_grand_total(self):
         t = myshop.get_grand_total()
+        self.assertEqual(t['food'], 30)
         self.assertEqual(t['home'], 25)
 
     def test_g_add_point(self):
-        """ Test add point """
-        self.assertEqual(myshop.add_data_point('lucas','food', 20180705, 10),0)
+        self.assertEqual(myshop.add_data_point('luc','food', 20180705, -20),0)
     
-    def test_h_get_grand_total_food(self):
-        """ Test total for food """
+    def test_h_get_grand_total(self):
         t = myshop.get_grand_total()
-        self.assertEqual(t['food'], 20)
-    
-    def test_i_get_grand_total_home(self):
-        """ Test total for home """
-        t = myshop.get_grand_total()
+        self.assertEqual(t['food'], 10)
         self.assertEqual(t['home'], 25)
 
-    def test_j_add_point(self):
-        """ Test add point """
-        self.assertEqual(myshop.add_data_point('lucas','food', 20180705, -30),0)
-    
-    def test_k_get_grand_total_food(self):
-        """ Test total for food """
-        t = myshop.get_grand_total()
-        self.assertEqual(t['food'], -10)
-    
-    def test_l_get_grand_total_home(self):
-        """ Test total for home """
-        t = myshop.get_grand_total()
+    def test_i_get_total_by_date(self):
+        t = myshop.get_total_by_date(min_date='20180701', max_date='20180731')
         self.assertEqual(t['home'], 25)
+        self.assertEqual(t['food'], 0)
+    
+    def test_j_get_total_by_date(self):
+        t = myshop.get_total_by_date(min_date='20180601', max_date='20180631')
+        self.assertEqual(t['home'], 0)
+        self.assertEqual(t['food'], 10)
+    
+    def test_k_get_total_by_date_user(self):
+        t = myshop.get_total_by_date(user='ana', min_date='20180601', max_date='20180631')
+        self.assertEqual(t['home'], 0)
+        self.assertEqual(t['food'], 10)
+        
+    def test_l_get_total_by_date_user(self):
+        t = myshop.get_total_by_date(user='ana', min_date='20180701', max_date='20180731')
+        self.assertEqual(t['home'], 0)
+        self.assertEqual(t['food'], 0)
+    
+    def test_m_get_total_by_date_user(self):
+        t = myshop.get_total_by_date(user='ana', min_date='20180601', max_date='20180731')
+        self.assertEqual(t['home'], 0)
+        self.assertEqual(t['food'], 10)
 
+    def test_n_get_total_by_date_user(self):
+        t = myshop.get_total_by_date(user='ana', min_date='20180601', max_date='20180731')
+        self.assertEqual(t['home'], 0)
+        self.assertEqual(t['food'], 10)
+
+    def test_o_grand_total_by_user(self):
+        t = myshop.get_grand_total(user='ana')
+        self.assertEqual(t['home'], 0)
+        self.assertEqual(t['food'], 10)
+    
+    def test_p_grand_total_by_user(self):
+        t = myshop.get_grand_total(user='luc')
+        self.assertEqual(t['home'], 25)
+        self.assertEqual(t['food'], 0)
 
 
 
